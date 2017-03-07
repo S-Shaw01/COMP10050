7c553eebb5af896204af76d1b25e004ee320c16c
S-Shaw01 <shanley.shaw@ucdconnect.ie>
2017-03-07 20:04:46 +0000

    Update partB.c

--- partB.c
+++ partB.c
@@ -9,10 +9,6 @@ struct Players
 {
 	char name[50];
 	char type[50];
-	char slotType[50];
-	int slotNum;
-	int numType;
-	int num;
 	int lifepoints;
 	int smartness;
 	int strength;
@@ -29,7 +25,11 @@ struct Slots
 	char type[DESCRIPTION];
 	int player;
 }slots[20];
-
+// im having a bit of trouble getting the players to work,ie, setting them as a specific type, entering them into an array and then being able to set read through the array
+//and set certain capabilities, there is prob some easy way of doing this but i just cant see it, the slot assignment seems to be working fine, sorry i dont
+//have more done but sadly im not the best coder out there. thought i would upload this so you could at least have some idea of what is done and by all 
+//means if you could think of some way of doing the players go for it becasue i cant for the life of me
+//i keep adding to this when think of differnet ways for  the players but so far none of my implementations have worked
 int main(void)
 {
 printf("------------------Player Information---------------\n");
